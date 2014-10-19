4bb84959c8938827b5dae75e2a412e7c3fe59f33
schimmelc <schimmelc4422@my.uwstout.edu>
2014-10-19 15:00:29 -0500

    Game Over Screen

---
+++ AllAmericanTrampolineShowdown/Assets/Scripts/GameOverScript.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameOverScript : MonoBehaviour {
+
+    public GameObject mainMenu;
+    public GameObject quit;
+
+    public void Menu () {
+        Application.LoadLevel("Menu");
+    }
+
+    public void Quit () {
+        Application.Quit();
+    }
+
+}
