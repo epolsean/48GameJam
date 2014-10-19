54acf510b4c433a8155027bf74d84aa707fb30bf
schimmelc <schimmelc4422@my.uwstout.edu>
2014-10-19 10:17:38 -0500

    boundary

--- AllAmericanTrampolineShowdown/Assets/MovePlayer.cs
+++ AllAmericanTrampolineShowdown/Assets/MovePlayer.cs
@@ -3,27 +3,38 @@ using System.Collections;
 
 public class MovePlayer : MonoBehaviour 
 {
+    Vector3 startPos = Vector3.zero;
 	// Use this for initialization
 	void Start () 
     {
-	    
+        startPos = transform.position;
 	}
 	
 	// Update is called once per frame
 	void Update () 
     {
         Vector3 newVec = transform.TransformDirection(Vector3.forward);
-
-        if (Input.GetAxis("Horizontal") == 1)
+        if (transform.position.x < startPos.x + 3.5)
         {
-            //GetComponent<Rigidbody>().AddForce(5*Vector3.forward);
-            GetComponent<Rigidbody>().velocity = new Vector3(newVec.x, GetComponent<Rigidbody>().velocity.y, GetComponent<Rigidbody>().velocity.z);
+            if (Input.GetAxis("Horizontal") == 1)
+            {
+                GetComponent<Rigidbody>().velocity = new Vector3(newVec.x, GetComponent<Rigidbody>().velocity.y, GetComponent<Rigidbody>().velocity.z);
+            }
         }
-        if (Input.GetAxis("Horizontal") == -1)
+        else
         {
-            //GetComponent<Rigidbody>().AddForce(-5 * Vector3.forward);
-
-            GetComponent<Rigidbody>().velocity = new Vector3(-1 * newVec.x, GetComponent<Rigidbody>().velocity.y, GetComponent<Rigidbody>().velocity.z);
+            GetComponent<Rigidbody>().AddForce(-20, 0, 0);
+        }
+        if (transform.position.x > startPos.x - 3.5)
+        {
+            if (Input.GetAxis("Horizontal") == -1)
+            {
+                GetComponent<Rigidbody>().velocity = new Vector3(-1 * newVec.x, GetComponent<Rigidbody>().velocity.y, GetComponent<Rigidbody>().velocity.z);
+            }
+        }
+        else
+        {
+            GetComponent<Rigidbody>().AddForce(20, 0, 0);
         }
 	}
 }
