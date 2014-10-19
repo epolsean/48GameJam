dc2992dd5ae8c6bfd65cacbcab7eeb88c5d61612
blochowcizr <blochowiczr@yahoo.com>
2014-10-19 16:24:50 -0500

    FinalFinal

--- AllAmericanTrampolineShowdown/Assets/GotKilled.cs
+++ AllAmericanTrampolineShowdown/Assets/GotKilled.cs
@@ -6,7 +6,7 @@ public class GotKilled : MonoBehaviour {
     public static bool DaEnd = false; 
     // Use this for initialization
 	void Start () {
-	
+        DaEnd = false; 
 	}
 	
 	// Update is called once per frame
